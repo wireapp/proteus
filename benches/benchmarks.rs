59e82b684e43f67c7e917edc9637f6516a4473e5
Mathieu Amiot <amiot.mathieu@gmail.com>
2021-12-16 17:09:44 +0100

    chore(fmt): fmt & clippy pass

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -11,24 +11,30 @@ fn bench_keygen(c: &mut Criterion) {
 fn bench_dh(c: &mut Criterion) {
     let x = KeyPair::new();
     let y = KeyPair::new();
-    c.bench_function("dh", |b| b.iter(|| {
-        let _sx = x.secret_key.shared_secret(&y.public_key);
-    }));
+    c.bench_function("dh", |b| {
+        b.iter(|| {
+            let _sx = x.secret_key.shared_secret(&y.public_key);
+        })
+    });
 }
 
 fn bench_sign(c: &mut Criterion) {
     let x = KeyPair::new();
-    c.bench_function("sign", |b| b.iter(|| {
-        let _s = x.secret_key.sign(b"foobarbaz");
-    }));
+    c.bench_function("sign", |b| {
+        b.iter(|| {
+            let _s = x.secret_key.sign(b"foobarbaz");
+        })
+    });
 }
 
 fn bench_verify(c: &mut Criterion) {
     let x = KeyPair::new();
     let s = x.secret_key.sign(b"foobarbaz");
-    c.bench_function("verify", |b| b.iter(|| {
-        let _r = x.public_key.verify(&s, b"foobarbaz");
-    }));
+    c.bench_function("verify", |b| {
+        b.iter(|| {
+            let _r = x.public_key.verify(&s, b"foobarbaz");
+        })
+    });
 }
 
 criterion_group!(benches, bench_keygen, bench_dh, bench_sign, bench_verify);
