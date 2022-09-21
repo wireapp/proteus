0c5596c539783684aeef40dea2f0c2a964edaa6b
Mathieu Amiot <amiot.mathieu@gmail.com>
2022-09-21 12:05:36 +0200

    feat!: Separated traits to another crate

--- src/session.rs
+++ src/session.rs
@@ -16,5 +16,4 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 pub use crate::internal::session::Error;
-pub use crate::internal::session::PreKeyStore;
 pub use crate::internal::session::Session;
