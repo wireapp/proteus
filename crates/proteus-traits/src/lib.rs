baabf8d6aa858cadd485034389944dcc440be05c
Mathieu Amiot <amiot.mathieu@gmail.com>
2022-09-21 18:51:43 +0200

    feat!: Proteus 2.0: Async edition

--- crates/proteus-traits/src/lib.rs
+++ crates/proteus-traits/src/lib.rs
@@ -1,12 +1,13 @@
 pub type RawPreKeyId = u16;
 pub type RawPreKey = Vec<u8>;
 
+#[async_trait::async_trait(?Send)]
 pub trait PreKeyStore {
     type Error;
 
     /// Lookup prekey by ID.
-    fn prekey(&mut self, id: RawPreKeyId) -> Result<Option<RawPreKey>, Self::Error>;
+    async fn prekey(&mut self, id: RawPreKeyId) -> Result<Option<RawPreKey>, Self::Error>;
 
     /// Remove prekey by ID.
-    fn remove(&mut self, id: RawPreKeyId) -> Result<(), Self::Error>;
+    async fn remove(&mut self, id: RawPreKeyId) -> Result<(), Self::Error>;
 }
